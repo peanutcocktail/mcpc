a46eb6e8bb11c97d399751001830d2cc34f4a98f
cocktailpeanut <cocktailpeanuts@proton.me>
2024-12-01 17:57:45 -0500

    update

--- applenotes.js
+++ applenotes.js
@@ -1,12 +1,10 @@
 module.exports = (options) => {
   return {
     "mcpServers": {
-      "apple-notes-mcp": {
-        "command": "uvx",
-        "args": [
-          "apple-notes-mcp"
-        ]
-      }
+      "command": "uvx",
+      "args": [
+        "apple-notes-mcp"
+      ]
     }
   }
 }
